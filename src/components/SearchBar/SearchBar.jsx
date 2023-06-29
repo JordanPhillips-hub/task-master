6464ce337b8fb330e6653306555bc94b0db1ef7c
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-06-28 21:12:22 -0600

    Create SearchBar component

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,30 @@
+import { CiSearch } from "react-icons/ci";
+import { BsArrowRightShort } from "react-icons/bs";
+import { SearchContainer, SearchIcon } from "./SearchBar.styled";
+import Input from "../Input/Input.styled";
+import Button from "../Button/Button";
+
+const SearchBar = () => {
+  return (
+    <SearchContainer>
+      <SearchIcon>
+        <CiSearch />
+      </SearchIcon>
+
+      <Input
+        htmlFor="search"
+        label="Search"
+        type="text"
+        id="search"
+        name="search"
+        placeholder="Search..."
+      />
+
+      <Button round type="button">
+        <BsArrowRightShort fontSize="1.5rem" />
+      </Button>
+    </SearchContainer>
+  );
+};
+
+export default SearchBar;
