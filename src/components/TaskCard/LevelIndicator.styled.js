61e95536e40a77bad5003c6e075b1113dd6fff49
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-06-28 21:17:32 -0600

    Create LevelIndicator.styled.js

---
+++ src/components/TaskCard/LevelIndicator.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const LevelIndicator = styled.div`
+  background-color: red;
+  width: 18px;
+  height: 18px;
+  border-radius: 50%;
+`
+export default LevelIndicator;
