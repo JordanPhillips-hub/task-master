66a08373f1526113cfc81bf0b7e942c3f036e0b5
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-06-28 21:18:45 -0600

    Create TaskLevel.jsx

---
+++ src/components/TaskLevel/TaskLevel.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/prop-types */
+import Header from "../Header/Header";
+import Button from "../Button/Button";
+import { FlexContainer } from "../../App.styles";
+
+const generateLevelButtons = (n, onClick) => {
+  return Array(n)
+    .fill()
+    .map((_, i) => (
+      <Button key={Math.random()} round sml type="button" onClick={onClick}>
+        {i + 1}
+      </Button>
+    ));
+};
+
+const TaskLevel = ({ text, onClick }) => {
+  return (
+    <section>
+      <Header text={text} />
+      <FlexContainer gap="11px">
+        {generateLevelButtons(10, onClick)}
+      </FlexContainer>
+    </section>
+  );
+};
+
+export default TaskLevel;
