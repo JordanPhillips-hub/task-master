b295934870d949b639321c1fc9b65592a89dcfa4
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-06-28 20:59:02 -0600

    Refactor App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import AddNewTask from "./pages/AddNewTask/AddNewTask";
+import { GlobalStyles } from "./App.styles";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const handleSubmit = (
+    taskName,
+    complexity,
+    priority,
+    subtask,
+    tag,
+    dueDate,
+    time
+  ) => {
+    const task = {
+      id: `${Math.random()}`,
+      taskName,
+      complexity,
+      priority,
+      subtask,
+      tag,
+      dueDate,
+      time,
+      completed: false,
+    };
+
+    const newList = [...tasks, task];
+    setTasks(newList);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <GlobalStyles />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home tasks={tasks} />}></Route>
+          <Route
+            exact
+            path="/AddNewTask"
+            element={<AddNewTask onSubmit={handleSubmit} />}
+          ></Route>
+        </Routes>
+      </Router>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
