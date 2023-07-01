ab704bcad563fd864a0ced67d0c5c9ee36eca1f6
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-06-30 18:29:21 -0600

    Create styled tag div

---
+++ src/components/Tag/Tag.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Tag = styled.div`
+  background-color: var(--clr-light-blue-transparent);
+  font-size: 0.75rem;
+  font-weight: 400;
+  padding: 0.375em 0.5em;
+  border-radius: 60px;
+`
+
+export default Tag;
