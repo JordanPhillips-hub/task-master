0a415c527690b820d30a093717a9e2b2638d3078
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-02 22:12:40 -0600

    Create style and active prop setting the background color to the active button

--- src/components/TaskLevel/TaskLevel.jsx
+++ src/components/TaskLevel/TaskLevel.jsx
@@ -3,22 +3,33 @@ import Header from "../Header/Header";
 import Button from "../Button/Button.styled";
 import { FlexContainer } from "../../App.styles";
 
-const generateLevelButtons = (n, onClick) => {
+const generateLevelButtons = (n, active, onClick) => {
   return Array(n)
     .fill()
     .map((_, i) => (
-      <Button key={Math.random()} round sml type="button" onClick={onClick}>
+      <Button
+        key={Math.random()}
+        style={
+          i + 1 === active
+            ? { backgroundColor: "var(--clr-sky-blue)" }
+            : undefined
+        }
+        round
+        sml
+        type="button"
+        onClick={onClick}
+      >
         {i + 1}
       </Button>
     ));
 };
 
-const TaskLevel = ({ text, onClick }) => {
+const TaskLevel = ({ text, active, onClick }) => {
   return (
     <section>
       <Header text={text} />
       <FlexContainer gap="11px">
-        {generateLevelButtons(10, onClick)}
+        {generateLevelButtons(10, active, onClick)}
       </FlexContainer>
     </section>
   );
