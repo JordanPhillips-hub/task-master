3d6be145542ff534cc6216eeaaeffdfd10466c57
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-04 13:48:40 -0600

    Refactor / clean up button styles

--- src/components/Button/Button.styled.js
+++ src/components/Button/Button.styled.js
@@ -1,6 +1,6 @@
 import styled, { css } from "styled-components";
 
-const Button = styled.button`
+export const sharedStyles = css`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -25,56 +25,64 @@ const Button = styled.button`
     padding: 0.857em 0 0.857em;
     border-radius: 60px;
   `}
+`
+export const ButtonPrimary = styled.button`
+ ${sharedStyles}
+  background-color: var(--clr-sky-blue);
+  color: var(--clr-white);
 
-  ${({ dark }) => dark && css`
-    background-color: var(--clr-sky-blue);
-    color: var(--clr-white);
+  &:hover,
+  &:focus {
+    background-color: var(--clr-deep-sky-blue);
+    border: 1px solid var(--clr-sky-blue);
+    box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.3);
+    transform: translateY(-3px);
+  }
 
-    &:hover,
-    &:focus {
-      background-color: var(--clr-deep-sky-blue);
-      border: 1px solid var(--clr-sky-blue);
-      box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.3);
-      transform: translateY(-3px);
+  ${({ remove }) => remove && css`
+    background-color: var(--clr-light-red-transparent);
+    &:hover {
+      background-color: var(--clr-red-orange);
+      border: none;
     }
-  `}
-
-  ${({ light }) => light && css`
-    background-color: var(--clr-white);
-    color: var(--clr-mostly-black);
-    border: 1px solid var(--clr-light-gray);
-
-    &:hover,
-    &:focus {
-      border: 1px solid var(--clr-deep-sky-blue);
-    }
-  `}
-
-  ${({ round }) => round && css`
-    background-color: ${({ light }) => light ? 'var(--clr-white)' : 'var(--clr-light-blue-transparent)'} };
-    padding: 0.25em 0.25em;
-    border-radius: 50%;
-    padding: 0.42em 0.42em;
-
-    &:hover,
-    &:focus {
-      transform: scale(1.1);
-      border: 1px solid var(--clr-deep-sky-blue);
-    }
-
-    ${({ sml }) => sml && css`
-      width: 2.143em;
-      height: 2.143em;
-    `}
-
-    ${({ remove }) => remove && css`
-      background-color: var(--clr-light-red-transparent);
-
-      &:hover {
-        border: 1px solid var(--clr-red-orange);
-      }
-    `}
   `}
 `
+export const ButtonSecondary = styled.button`
+  ${sharedStyles}
+  background-color: var(--clr-white);
+  color: var(--clr-mostly-black);
+  border: 1px solid var(--clr-light-gray);
 
-export default Button;
+  &:hover,
+  &:focus {
+    border: 1px solid var(--clr-deep-sky-blue);
+  }
+`
+export const ButtonRound = styled.button`
+  ${sharedStyles}
+  background-color: ${({ light }) => light
+    ? 'var(--clr-white)'
+    : 'var(--clr-light-blue-transparent)'};
+  border-radius: 50%;
+  padding: 0.25em 0.25em;
+  padding: 0.42em 0.42em;
+
+  &:hover,
+  &:focus {
+    transform: scale(1.1);
+    border: 1px solid var(--clr-deep-sky-blue);
+  }
+
+  ${({ sml }) => sml && css`
+    width: 2.143em;
+    height: 2.143em;
+  `}
+
+  ${({ remove }) => remove && css`
+    background-color: var(--clr-light-red-transparent);
+    &:hover {
+      background-color: var(--clr-red-orange);
+      border: none;
+    }
+  `}
+`
