84abb6bae9ad8b1d1ac4fedcc73e206c95d46938
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-05 11:50:52 -0600

    Remove InputContainer component from SearchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,22 +2,19 @@ import { SearchContainer, SearchIcon } from "./SearchBar.styled";
 import Input from "../Input/Input";
 import Icon from "../Icon/Icon";
 import Button from "../Button/Button";
-import { InputContainer } from "../../App.styles";
 
 const SearchBar = () => {
   return (
     <SearchContainer>
-      <InputContainer>
-        <SearchIcon>
-          <Icon type="search" />
-        </SearchIcon>
+      <SearchIcon>
+        <Icon type="search" />
+      </SearchIcon>
 
-        <Input label="Search" id="search" placeholder="Search..." />
+      <Input label="Search" id="search" placeholder="Search..." />
 
-        <Button variant="round">
-          <Icon type="arrowRight" />
-        </Button>
-      </InputContainer>
+      <Button variant="round">
+        <Icon type="arrowRight" />
+      </Button>
     </SearchContainer>
   );
 };
