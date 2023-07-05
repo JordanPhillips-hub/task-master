b099bbb4b356c4df03c7b9f444da45f758db0aeb
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-05 11:39:14 -0600

    Create StyledPageHeader

---
+++ src/components/Header/PageHeader.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const StyledPageHeader = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+
+  h1 {
+    margin-left: 73px;
+  }
+`
+export default StyledPageHeader;
