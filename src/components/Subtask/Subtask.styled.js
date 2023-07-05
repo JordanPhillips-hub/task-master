b46bd321edd45ba366e6887bf4f158c5c444a287
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-05 10:21:28 -0600

    Create StyledSubtask

---
+++ src/components/Subtask/Subtask.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const StyledSubtask = styled.div`
+  background-color: var(--clr-white);
+  padding: 0.85em 10.6em 0.85em 3em;  
+  border-radius: 90px;
+  width: 100%;
+`
+
+export default StyledSubtask;
