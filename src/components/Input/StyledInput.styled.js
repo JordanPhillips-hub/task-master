0d7aa75e5bf38a1418fac72e99730c75efd0af3f
Jordan Phillips <jordan.phillips.988@gmail.com>
2023-07-05 23:46:28 -0500

    Remove radio input styles

--- src/components/Input/StyledInput.styled.js
+++ src/components/Input/StyledInput.styled.js
@@ -17,34 +17,6 @@ const StyledInput = styled.input`
   width: 100%;
   padding: 1em 10.6em 1em 3em;
 }
-
-&[type=radio] {
-  background-color: var(--clr-white);
-  color: var(--clr-black);
-  appearance: none;
-  font: inherit;
-  display: grid;
-  place-content: center;
-}
-
-&[type=radio],
-&[type=radio]::before {
-  width: 1.22em;
-  height: 1.22em;
-  border: 1px solid var(--clr-black);
-  border-radius: 50%;
-}
-
-&[type=radio]::before {
-  content: "";
-  transform: scale(0);
-  transition: 120ms transform ease-in-out;
-  box-shadow: inset 1em 1em var(--clr-black);
-}
-
-&[type=radio]:checked::before {
-  transform: scale(0.6);
-}
 `
 
 export default StyledInput;
