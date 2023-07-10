4a6099b641b189f8cfb011335868651a4bbe16a7
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-10 12:14:06 -0600

    Refactor TaskDetails
    Import useContext, useParams, Link and TaskContext
    
    Destructure tasks from TaskContext and id from useParams
    
    Create the task variable that returns the task.id that is eqaul to the useParams id
    
    Create boilerplate jsx

--- src/pages/TaskDetails/TaskDetails.jsx
+++ src/pages/TaskDetails/TaskDetails.jsx
@@ -1,5 +1,24 @@
+// /* eslint-disable react/prop-types */
+import { useContext } from "react";
+import { useParams, Link } from "react-router-dom";
+import { TaskContext } from "../../contexts/TaskContext";
+
 const TaskDetails = () => {
-  return <h1>Task Details</h1>
+  const { tasks } = useContext(TaskContext);
+  const { id } = useParams();
+  const task = tasks.find((task) => task.id === id);
+
+  return (
+    <>
+      <Link to="/">
+        <h1>Go Home</h1>
+      </Link>
+
+      <div>
+        <h2>{task.taskName}</h2>
+      </div>
+    </>
+  );
 };
 
 export default TaskDetails;
