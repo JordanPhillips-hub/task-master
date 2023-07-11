f3e04cf11b0b557f29e433e438cd072d47a8b210
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-11 07:43:36 -0600

    Create utils folder
    Create formatTime file / function

---
+++ src/utils/formatTime.js
@@ -0,0 +1,11 @@
+export const formatTime = (timeValue) => {
+  let [hours, minutes] = timeValue.split(":");
+  let period = "AM";
+  if (hours > 12) {
+    hours -= 12;
+    period = "PM";
+  }
+  if (hours < 10) hours = Number(hours);
+  timeValue = `${hours}:${minutes} ${period}`;
+  return timeValue;
+};
