61547257dad70936e09f692fc4d0a81c2a7be14c
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-11 10:24:32 -0600

    Create setWarningColor function, change background color prop to warningColor

--- src/components/Task/TaskWarning/TaskWarning.styled.js
+++ src/components/Task/TaskWarning/TaskWarning.styled.js
@@ -1,7 +1,17 @@
 import styled from "styled-components";
 
+export const setWarningColor = (priority) => {
+  let color =
+    priority <= 4
+      ? "var(--clr-sky-blue)"
+      : priority <= 7
+        ? "var(--clr-bright-orange)"
+        : "var(--clr-red-orange)";
+  return color;
+};
+
 const TaskWarning = styled.div`
-  background-color: ${({ priorityColor }) => priorityColor};
+  background-color: ${({ warningColor }) => warningColor};
   width: 18px;
   height: 18px;
   border-radius: 50%;
