7691fba53b38dec83bf4e222a7b68c3bf09a6e79
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-11 10:03:46 -0600

    Create PageHeader

---
+++ src/components/PageHeader/PageHeader.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
+import StyledPageHeader from "./PageHeader.styled";
+import Header from "src/components/Header/Header";
+import Button from "src/components/Button/Button";
+import Icon from "src/components/Icon/Icon";
+
+const PageHeader = ({ text }) => {
+  return (
+    <StyledPageHeader>
+      <Link to="/">
+        <Button variant="round" light>
+          <Icon type="arrowLeft" />
+        </Button>
+      </Link>
+
+      <Header lrg text={text} />
+    </StyledPageHeader>
+  );
+};
+
+export default PageHeader;
