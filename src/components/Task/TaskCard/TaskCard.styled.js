ee1672792976861161d4946e72c95a9cb12be711
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-11 10:05:46 -0600

    Create styled TaskCard

---
+++ src/components/Task/TaskCard/TaskCard.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledTaskCard = styled.div`
+  background-color: var(--clr-white);
+  margin-bottom: 24px;
+  padding: 20px 14px;
+  border-radius: 18px;
+`
+export default StyledTaskCard;
