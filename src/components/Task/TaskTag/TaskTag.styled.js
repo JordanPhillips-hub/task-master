a3ba6ac5051954ccbffe422a19c423781663608d
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-11 10:07:09 -0600

    Create TaskTag

---
+++ src/components/Task/TaskTag/TaskTag.styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const tagColors =
+  ["var(--clr-light-beige)", "var(--clr-mint-green)", "var(--clr-light-cyan)"];
+
+const TaskTag = styled.div`
+  background-color: ${() => tagColors[Math.floor(Math.random() * tagColors.length)]};
+  font-size: 0.75rem;
+  font-weight: 400;
+  padding: 0.375em 0.5em;
+  border-radius: 60px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+`
+
+export default TaskTag;
