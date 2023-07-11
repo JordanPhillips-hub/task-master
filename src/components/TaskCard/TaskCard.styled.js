9d36b77e20bf2fbbacc1dfdb20fece43c20ff182
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-10 23:21:48 -0600

    Remove h2, p, span and dueDate styles

--- src/components/TaskCard/TaskCard.styled.js
+++ src/components/TaskCard/TaskCard.styled.js
@@ -5,25 +5,5 @@ const StyledTaskCard = styled.div`
   margin-bottom: 24px;
   padding: 20px 14px;
   border-radius: 18px;
-
-  h2 {
-    font-size: 1rem;
-    margin: 0;
-  }
-
-  p {
-    font-size: 0.875rem;
-    color: var(--clr-dim-gray);
-    margin: 0;
-  }
-
-  span {
-    color: var(--clr-black);
-    font-weight: 500;
-  }
-
-  .dueDate {
-    color: ${({ priorityColor }) => priorityColor || ''};
-  }
 `
 export default StyledTaskCard;
