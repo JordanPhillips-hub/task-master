2c47ee4d9d1a0428d9ca57c151e74f44d9587c92
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-12 09:55:23 -0600

    Change margin bottom to 10px

--- src/components/Subtask/Subtask.styled.js
+++ src/components/Subtask/Subtask.styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const StyledSubtask = styled.li`
   background-color: var(--clr-white);
-  margin-bottom: 6px;
+  margin-bottom: 10px;
   padding: 0.85em 10.6em 0.85em 3em;  
   border-radius: 90px;
   width: 100%;
