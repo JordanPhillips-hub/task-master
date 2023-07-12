e9243b1103153fbdf574e03c1d698ba47bffb20b
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-12 09:34:30 -0600

    Add styles
    Add margin bottom and a position and left styles to button

--- src/components/Subtask/Subtask.styled.js
+++ src/components/Subtask/Subtask.styled.js
@@ -1,10 +1,15 @@
 import styled from "styled-components";
 
-const StyledSubtask = styled.div`
+const StyledSubtask = styled.li`
   background-color: var(--clr-white);
+  margin-bottom: 6px;
   padding: 0.85em 10.6em 0.85em 3em;  
   border-radius: 90px;
   width: 100%;
-`
 
+  button:first-of-type {
+    position: absolute;
+    left: 167%;
+  }
+`
 export default StyledSubtask;
