03602e67c2d30ff1dd754790f48e4f6f30dcbd77
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-12 21:09:27 -0600

    Create styles for completed subtask

--- src/components/Subtask/Subtask.styled.js
+++ src/components/Subtask/Subtask.styled.js
@@ -6,10 +6,16 @@ const StyledSubtask = styled.li`
   padding: 0.85em 10.6em 0.85em 3em;  
   border-radius: 90px;
   width: 100%;
+  text-decoration: ${({ complete }) => complete ? 'line-through' : 'none'};
 
-  button:first-of-type {
+  button {
+    background-color: ${({ complete }) => complete ? 'var(--clr-sky-blue)' : 'var(--clr-light-blue-transparent)'};
     position: absolute;
     left: 167%;
+  }
+
+  svg {
+    color: ${({ complete }) => complete ? 'var(--clr-white)' : 'var(--clr-black)'}
   }
 `
 export default StyledSubtask;
