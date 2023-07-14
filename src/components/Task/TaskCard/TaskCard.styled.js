8a7d44ae724eea5803be66ab665ea28db5208ae2
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-13 21:24:32 -0600

    Change padding, add styles for small and a tags

--- src/components/Task/TaskCard/TaskCard.styled.js
+++ src/components/Task/TaskCard/TaskCard.styled.js
@@ -3,7 +3,21 @@ import styled from "styled-components";
 const StyledTaskCard = styled.div`
   background-color: var(--clr-white);
   margin-bottom: 24px;
-  padding: 20px 14px;
+  padding: 20px 14px 6px 14px;
   border-radius: 18px;
+
+  small {
+    display: inline-block;
+    margin-top: 15px;
+    text-decoration: underline;
+  }
+
+  a {
+    &:hover,
+    &:focus {
+      outline: none;
+      color: var(--clr-black);
+    }
+  }
 `
 export default StyledTaskCard;
