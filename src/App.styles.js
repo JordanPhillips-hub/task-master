caaa29c5f48563d56297a5b0b77bfa71beec4de1
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-13 22:09:04 -0600

    Create variables for light green and pale green

--- src/App.styles.js
+++ src/App.styles.js
@@ -12,6 +12,8 @@ export const GlobalStyles = createGlobalStyle`
   --clr-dim-gray: hsl(0, 0%, 44%);
   --clr-light-beige: hsl(37, 100%, 95%);
   --clr-pale-coral: hsl(7, 100%, 96%);
+  --clr-pale-green: 	hsl(120, 100%, 99%);
+  --clr-light-green: hsl(120, 100%, 95%);
   --clr-mint-green: hsl(110, 100%, 95%);
   --clr-light-cyan: hsl(183, 100%, 95%);
   --clr-light-blue-gray: hsl(205, 57%, 92%); 
