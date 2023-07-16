72d4611663270eb9fd03a56e7942fd5b3e5c2611
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-16 10:29:48 -0600

    Create value and onChange props

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,16 +1,23 @@
+/* eslint-disable react/prop-types */
 import { SearchContainer, SearchIcon } from "./SearchBar.styled";
 import Input from "src/components/Input/Input";
 import Icon from "src/components/Icon/Icon";
 import Button from "src/components/Button/Button";
 
-const SearchBar = () => {
+const SearchBar = ({ value, onChange }) => {
   return (
     <SearchContainer>
       <SearchIcon>
         <Icon type="search" />
       </SearchIcon>
 
-      <Input label="Search" id="search" placeholder="Search..." />
+      <Input
+        label="Search"
+        id="search"
+        placeholder="Search..."
+        value={value}
+        onChange={onChange}
+      />
 
       <Button variant="round">
         <Icon type="arrowRight" />
