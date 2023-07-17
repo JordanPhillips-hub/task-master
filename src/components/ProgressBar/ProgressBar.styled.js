7e145a660d04ad847d048fd136c97e7fcb33c9bd
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-17 10:21:12 -0600

    Create styles for round progress bar / Update FlatProgressBar styles

--- src/components/ProgressBar/ProgressBar.styled.js
+++ src/components/ProgressBar/ProgressBar.styled.js
@@ -1,19 +1,44 @@
 import styled from "styled-components";
 
-const StyledProgressBar = styled.div`
+export const FlatProgressBar = styled.div`
   background-color: hsla(0, 0%, 18%, 0.05);
   width: 100%;
   height: 0.75em;
   border-radius: 33px;
 
   && > div {
-    background-color: var(--clr-sky-blue);
+    background-color: ${({ warningColor }) => warningColor};
     height: 100%;
     border-radius: 33px;
     transition: width 0.5s ease-out;
   }
 `
+export const RoundProgressBar = styled.div`
+  position: absolute;
+  right: -10px;
+  bottom: 20px;
 
-export default StyledProgressBar;
+  .progress-ring {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .progress-ring__background {
+    stroke: hsl(0, 0%, 88%);
+  }
+
+  .progress-ring__circle {
+    stroke: ${({ warningColor }) => warningColor};
+    transition: stroke-dashoffset 0.5s ease-in-out;
+  }
+
+  .progress-ring__text {
+    fill: var(--clr-black);
+    font-size: 0.75rem;
+  }
+`
+
+
 
 
