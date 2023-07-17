f645212662fbb6e86459ee70e9f059d9a9b60a5f
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-17 10:49:20 -0600

    Create input and page header for EditTask component

--- src/pages/EditTask/EditTask.jsx
+++ src/pages/EditTask/EditTask.jsx
@@ -1,7 +1,29 @@
+import { useContext } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { TaskContext } from "../../contexts/TaskContext";
+import Input from "src/components/Input/Input";
+import PageHeader from "src/components/PageHeader/PageHeader";
+import { Main } from "src/App.styles";
+
 const EditTask = () => {
+  const { tasks } = useContext(TaskContext);
+  const { id } = useParams();
+  const task = tasks.find((task) => task.id === id);
   return (
+    <Main>
+      <PageHeader text="Edit Task" />
+      <section>
+        <Input
+          label="Add Task"
+          id="taskName"
+          value={task.taskName}
+          placeholder="Task 1..."
+          required={true}
+          // onChange={handleChange}
+        />
+      </section>
+    </Main>
+  );
+};
 
-  )
-}
-
-export default EditTask
+export default EditTask;
