a97dfbbf5ae6fa300ed34065d88179a55d96da63
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-17 11:54:58 -0600

    Refactor EditTask
    Create state for isEditing and editedTaskName
    
    Create handleIsEditing, handleChange and handleSaveChanges functions
    
    Create conditional render for isEditing returning a span, Change value of input to be editedTaskName
    
    Create save changes button

--- src/pages/EditTask/EditTask.jsx
+++ src/pages/EditTask/EditTask.jsx
@@ -1,27 +1,51 @@
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { TaskContext } from "../../contexts/TaskContext";
 import Input from "src/components/Input/Input";
 import PageHeader from "src/components/PageHeader/PageHeader";
+import Button from "src/components/Button/Button";
 import { Main } from "src/App.styles";
 
 const EditTask = () => {
-  const { tasks } = useContext(TaskContext);
+  const navigate = useNavigate();
+  const { tasks, editTask } = useContext(TaskContext);
   const { id } = useParams();
   const task = tasks.find((task) => task.id === id);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTaskName, setEditedTaskName] = useState(task.taskName);
+
+  const handleIsEditing = () => {
+    setIsEditing(true);
+  };
+
+  const handleChange = (e) => {
+    setEditedTaskName(e.target.value);
+  };
+
+  const handleSaveChanges = () => {
+    editTask(id, editedTaskName);
+    setIsEditing(false);
+    navigate("/");
+  };
+
   return (
     <Main>
       <PageHeader text="Edit Task" />
       <section>
-        <Input
-          label="Add Task"
-          id="taskName"
-          value={task.taskName}
-          placeholder="Task 1..."
-          required={true}
-          // onChange={handleChange}
-        />
+        {isEditing ? (
+          <span onClick={handleIsEditing}>{task.value}</span>
+        ) : (
+          <Input
+            label="Add Task"
+            id="taskName"
+            value={editedTaskName}
+            placeholder="Task 1..."
+            required={true}
+            onChange={handleChange}
+          />
+        )}
       </section>
+      <Button onClick={handleSaveChanges}>Save Changes</Button>
     </Main>
   );
 };
