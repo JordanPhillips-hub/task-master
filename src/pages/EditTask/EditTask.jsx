a8630fb5a6b5b6c4edecaabdb78048f751b54e1b
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-17 20:40:10 -0600

    Refactor EditTask
    Create state for editedTaskLevel
    
    Include editedTaskLevels complexity and priority to handleSubmit
    
    Create handleTaskLevel function setting the edited task level state
    
    Wrap jsx in form tag and create the LevelSelector section

--- src/pages/EditTask/EditTask.jsx
+++ src/pages/EditTask/EditTask.jsx
@@ -1,9 +1,10 @@
 import { useState, useContext } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { TaskContext } from "../../contexts/TaskContext";
+import { TaskContext } from "src/contexts/TaskContext";
 import Input from "src/components/Input/Input";
-import PageHeader from "src/components/PageHeader/PageHeader";
 import Button from "src/components/Button/Button";
+import PageHeader from "src/components/PageHeader/PageHeader";
+import LevelSelector from "src/components/LevelSelector/LevelSelector";
 import { Main } from "src/App.styles";
 
 const EditTask = () => {
@@ -13,6 +14,10 @@ const EditTask = () => {
   const task = tasks.find((task) => task.id === id);
   const [isEditing, setIsEditing] = useState(false);
   const [editedTaskName, setEditedTaskName] = useState(task.taskName);
+  const [editedTaskLevel, setEditedTaskLevel] = useState({
+    complexity: 0,
+    priority: 0,
+  });
 
   const handleIsEditing = () => {
     setIsEditing(true);
@@ -22,30 +27,56 @@ const EditTask = () => {
     setEditedTaskName(e.target.value);
   };
 
-  const handleSaveChanges = () => {
-    editTask(id, editedTaskName);
+  const handleSubmit = () => {
+    editTask(id, editedTaskName, editedTaskLevel.complexity, editedTaskLevel.priority);
     setIsEditing(false);
     navigate("/");
   };
 
+  const handleTaskLevel =
+    (levelType) =>
+    ({ target: { innerText } }) => {
+      setEditedTaskLevel((prevState) => ({
+        ...prevState,
+        [levelType]: Number(innerText),
+      }));
+    };
+
   return (
     <Main>
-      <PageHeader text="Edit Task" />
-      <section>
-        {isEditing ? (
-          <span onClick={handleIsEditing}>{task.value}</span>
-        ) : (
-          <Input
-            label="Add Task"
-            id="taskName"
-            value={editedTaskName}
-            placeholder="Task 1..."
-            required={true}
-            onChange={handleChange}
+      <form onSubmit={handleSubmit}>
+        <PageHeader text="Edit Task" />
+        <section>
+          {isEditing ? (
+            <span onClick={handleIsEditing}>{task.value}</span>
+          ) : (
+            <Input
+              label="Add Task"
+              id="taskName"
+              value={editedTaskName}
+              placeholder="Task 1..."
+              required={true}
+              onChange={handleChange}
+            />
+          )}
+        </section>
+
+        <section>
+          <LevelSelector
+            text="Select Complexity Level"
+            onClick={handleTaskLevel("complexity")}
+            active={task.complexity}
           />
-        )}
-      </section>
-      <Button onClick={handleSaveChanges}>Save Changes</Button>
+          <LevelSelector
+            text="Select Priority Level"
+            onClick={handleTaskLevel("priority")}
+            active={task.priority}
+          />
+        </section>
+        <Button lrg width="50%" type="submit">
+          Save Task
+        </Button>
+      </form>
     </Main>
   );
 };
