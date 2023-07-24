b403d63f16737f74e732fe25a9574e7934ba7fd5
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-24 11:13:59 -0600

    Refactor colors to use more reusable names / Add tag colors

--- src/App.styles.js
+++ src/App.styles.js
@@ -5,27 +5,44 @@ export const GlobalStyles = createGlobalStyle`
 @import url("https://fonts.googleapis.com/css2?family=Inter:wght@400;500&family=Manrope&family=Poppins:wght@500&display=swap");
 
 :root {
-  --clr-white: hsl(0, 0%, 100%);
-  --clr-off-white: hsl(0, 0%, 96%);
-  --clr-very-light-gray: hsl(0, 0%, 85%);
-  --clr-progress-bar-bg: hsl(0, 0%, 88%);
-  --clr-light-gray: hsl(0, 0%, 89%);
-  --clr-dim-gray: hsl(0, 0%, 44%);
-  --clr-light-beige: hsl(37, 100%, 95%);
-  --clr-pale-coral: hsl(7, 100%, 96%);
-  --clr-pale-green: 	hsl(120, 100%, 97%);
-  --clr-light-green: hsl(120, 100%, 95%);
-  --clr-mint-green: hsl(110, 100%, 95%);
-  --clr-light-cyan: hsl(183, 100%, 95%);
-  --clr-light-blue-gray: hsl(205, 57%, 92%); 
-  --clr-black: hsl(0, 0%, 0%);
-  --clr-red-orange: hsl(4, 100%, 60%);
-  --clr-bright-orange: hsl(29, 99%, 51%);
-  --clr-navy-blue: hsl(217, 100%, 17%);
-  --clr-deep-sky-blue:  hsl(205, 100%, 40%);
-  --clr-sky-blue: hsl(205, 100%, 53%);
-  --clr-light-blue-transparent: hsla(205, 100%, 53%, 0.1);
-  --clr-light-red-transparent: hsla(4, 100%, 60%, 0.1);
+    /* Neutral Colors */
+  --neutral-100: hsl(0, 0%, 100%); // Pure white 
+  --neutral-200: hsl(0, 0%, 96%); // Off-white 
+  --neutral-300: hsl(0, 0%, 88%); // Lighter gray
+  --neutral-400: hsl(0, 0%, 89%); // Light gray 
+  --neutral-500: hsl(0, 0%, 44%); // Dim gray 
+  --neutral-900: hsl(0, 0%, 0%);  // Black 
+
+  /* Primary Colors */
+  --primary-100: hsl(205, 100%, 53%); // Sky blue 
+  --primary-200: hsl(29, 99%, 51%); // Bright orange 
+  --primary-300: hsl(4, 100%, 60%); // Red orange 
+  --primary-400: hsl(205, 100%, 40%); // Deep sky blue 
+
+  /* Secondary Colors */
+  --secondary-100: hsl(120, 100%, 97%); // Pale green 
+  --secondary-200: hsl(120, 100%, 95%); // Light green 
+
+  /* Transparent Colors */
+  --trans-100: hsla(205, 100%, 53%, 0.1); // Light blue transparent 
+  --trans-200: hsla(4, 100%, 60%, 0.1);   // Red transparent 
+
+  /* Tag Colors */
+   --tag-100: hsl(37, 100%, 93%);  // Light beige 
+   --tag-200: hsl(110, 100%, 93%); // Mint green 
+   --tag-300: hsl(183, 100%, 93%); // Light cyan 
+   --tag-400: hsl(10, 100%, 93%); // Red
+   --tag-500: hsl(270, 100%, 93%); // Purple
+   --tag-600: hsl(50, 100%, 93%); // Orangish
+   --tag-700: hsl(80, 100%, 93%); // Yellowish
+   --tag-800: hsl(150, 100%, 93%); // Teal
+   --tag-900: hsl(190, 100%, 93%); // Cyan
+   --tag-1000: hsl(255, 100%, 93%); // Light Blue
+   --tag-1100: hsl(220, 100%, 93%); // Blue
+   --tag-1200: hsl(300, 100%, 93%); // Magenta
+   --tag-1300: hsl(330, 100%, 93%); // Pink
+   --tag-1400: hsl(25, 100%, 93%); // Orange
+   --tag-1500: hsl(63, 100%, 93%); // Golden Yellow
 }
 
 *,
@@ -39,7 +56,7 @@ html:focus-within {
 }
 
 body { 
-  background-color: var(--clr-off-white);
+  background-color: var(--neutral-200);
   font-family: "Inter", sans-serif;
   font-weight: 400;
   line-height: 1.5; 
@@ -53,7 +70,7 @@ h1 {
 
 h1,
 h2 {
-  color: var(--clr-black);
+  color: var(--neutral-900);
   font-weight: 500;
 }
 
@@ -65,7 +82,6 @@ h2 {
 ul, 
 ol { 
   list-style: none;
-  padding: 0;
   margin: 0;
 }
 
@@ -80,7 +96,7 @@ input:focus {
 }
 
 a {
-  color: var(--clr-sky-blue);
+  color: var(--primary-100);
   text-decoration: none;
 }
 `
