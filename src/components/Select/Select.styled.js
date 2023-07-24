ff84d103447006d338f7d7d4a66e56cfabca8da1
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-24 16:38:33 -0600

    Add padding to li

--- src/components/Select/Select.styled.js
+++ src/components/Select/Select.styled.js
@@ -5,18 +5,16 @@ background-color: var(--neutral-100);
 position: absolute;
 width: 185px;
 margin-top: 5px;
-padding: 13px 14px;
 border-radius: 14px;
 box-shadow: 0px 16px 45px rgba(0, 0, 0, 0.16);
 z-index: 100;
 
-  li {
+li {
   display: flex;
   align-items: center;
   justify-content: space-between;
   border-bottom: 1px solid var(--neutral-900);
-  padding-top: 9px;
-  padding-bottom: 9px;
+  padding: 9px;
 
   small {
     font-size: 0.75rem;
@@ -32,7 +30,7 @@ z-index: 100;
   
   &:last-child {
     border-bottom: 0px;
-    padding-bottom: 0;
+    padding-bottom: 9px;
   }
 }
 `
