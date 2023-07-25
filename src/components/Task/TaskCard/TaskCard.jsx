7fb9073b6615750a912892dd1c5ed40c10722d0e
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-25 11:39:24 -0600

    Remove motion animations for TaskCard

--- src/components/Task/TaskCard/TaskCard.jsx
+++ src/components/Task/TaskCard/TaskCard.jsx
@@ -1,22 +1,8 @@
 /* eslint-disable react/prop-types */
 import StyledTaskCard from "./TaskCard.styled";
-import { motion } from "framer-motion";
 
 const TaskCard = ({ children, complete }) => {
-  const variants = {
-    hidden: { y: 200, opacity: 0 },
-    visible: { y: 0, opacity: 1 },
-  };
-  return (
-    <motion.div
-      initial="hidden"
-      animate="visible"
-      variants={variants}
-      transition={{ duration: 0.5, type: "spring", stiffness: 80 }}
-    >
-      <StyledTaskCard complete={complete}>{children}</StyledTaskCard>
-    </motion.div>
-  );
+  return <StyledTaskCard complete={complete}>{children}</StyledTaskCard>;
 };
 
 export default TaskCard;
