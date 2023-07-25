62a6c9cf276ae2619d5d836830fa26c8959f0f77
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-07-24 20:57:30 -0600

    Refactor task tag styles

--- src/components/Task/TaskTag/TaskTag.styled.js
+++ src/components/Task/TaskTag/TaskTag.styled.js
@@ -2,11 +2,15 @@ import styled from "styled-components";
 import { tagColors } from './TagColors'
 
 const TaskTag = styled.div`
-  background-color: ${() => tagColors[Math.floor(Math.random() * tagColors.length)]};
-  font-size: 0.75rem;
-  font-weight: 400;
-  padding: 0.375em 0.5em;
-  border-radius: 60px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+ background-color: ${() => tagColors[Math.floor(Math.random() * tagColors.length)]};
+ font-size: 0.75rem;
+ font-weight: 400;
+ display: flex;
+ align-items: center;
+ justify-content: center;
+ min-width: 40px;
+ padding: 0.1em 0.5em;
+ border-radius: 0.3em;
+ box-shadow: inset 0 -0.075em 0 rgba(0,0,0,0.2);
 `
 export default TaskTag;
