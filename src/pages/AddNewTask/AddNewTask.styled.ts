0c871e7960e81f7ce4d465dfbaf9b893dd43ee3d
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-08-06 00:07:08 -0600

    Refactor styles

--- src/pages/AddNewTask/AddNewTask.styled.ts
+++ src/pages/AddNewTask/AddNewTask.styled.ts
@@ -1,36 +1,34 @@
 import styled from "styled-components";
 
 const StyledAddNewTask = styled.form`
+  && ul {
+    padding: 0;
+  }
 
-&& ul {
-  padding: 0 ;
-}
+  label {
+    font-size: 1rem;
+    font-weight: 500;
+    display: block;
+    margin: 0.83em 0;
+  }
 
-label {
-  font-size: 1.125rem;
-  font-weight: 500;
-  display: block;
-  margin: 0.83em 0;
- }
+  section:nth-child(4) div {
+    flex: 1;
+  }
 
- section:nth-child(4) div {
-  flex: 1;
- }
+  section:nth-child(5) > div button {
+    position: absolute;
+    right: 3%;
+  }
 
- section:nth-child(5) > div button {
-  position: absolute;
-  right: 3%;
- }
-
-  section:nth-child(6)  {
+  section:nth-child(6) {
     margin-bottom: 30px;
- }
+  }
 
-
- input[type=date],
- input[type=time] {
-  width: 100%;
-  padding: 12px 24px;
- }
-`
+  input[type="date"],
+  input[type="time"] {
+    width: 100%;
+    padding: 12px 24px;
+  }
+`;
 export default StyledAddNewTask;
